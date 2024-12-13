0ac26fa0f7fe9177ec5c77ddd6a1b9e44ef107bf
Nicoly Alcantara Moraes <nicoly.alcantara23@gmail.com>
2024-12-13 10:55:27 -0300

    Fix sequence migration

---
+++ food/src/main/resources/db/migration/V3__fix_sequences_migration.sql
@@ -0,0 +1,4 @@
+ALTER SEQUENCE public.users_id_seq RESTART 6;
+ALTER SEQUENCE public.restaurants_id_seq RESTART 6;
+ALTER SEQUENCE public.categories_id_seq RESTART 26;
+ALTER SEQUENCE public.products_id_seq RESTART 126;
